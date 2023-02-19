deebfa18f1e66db5cbf782676b6484f9899db85b
lochinbek9 <lochinbeksetor@gmail.com>
2023-02-19 17:17:15 +0500

    loader added

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
-
 "use strict";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const loader = document.querySelector(".lds-hourglass");
+  setTimeout(() => {
+    loader.style.opacity = "0";
+    loader.style.display = "none";
+  }, 5000);
+});
